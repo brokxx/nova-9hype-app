18356abf804d7e1c5178b83ed2edd9d767818870
Manao Dardailler <manaocode@gmail.com>
2026-06-24 13:52:25 +0200

    affichage adresse wallet

---
+++ assets/shouldProceedtoEmbeddedWalletCreationFlow-CBt9hKD6-B0I5RctM.js
@@ -0,0 +1,1 @@
+import{d$ as o,e0 as r}from"./app-DnBHHgGn.js";const n=(e,a)=>o(e,a.ethereum.createOnLogin)||r(e,a.solana.createOnLogin);export{n as a};
--- assets/shouldProceedtoEmbeddedWalletCreationFlow-CBt9hKD6-_2qSPIps.js
+++
@@ -1,1 +0,0 @@
-import{d$ as o,e0 as r}from"./app-Dvg1b3kT.js";const n=(e,a)=>o(e,a.ethereum.createOnLogin)||r(e,a.solana.createOnLogin);export{n as a};
